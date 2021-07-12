f8a98a1dc7e96a8169af47eb42612e238a633269
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-12 06:17:38 -1000

    clean index

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,7 @@ import { useState } from "react";
 
 function Index() {
   const [text, setText] = useState("Welcome to Next.js!");
-  return (
-    <div
-      onClick={() => {
-        axios
-          .get(`/api/test/reviewCalendarUpTo1Week`)
-          .then((res) => {
-            console.log(res, "what came back?");
-            // if (res.data) {
-            //   setText(res.data);
-            // } else {
-            //   console.log("res error");
-            // }
-          })
-          .catch((err) => {
-            console.log("an error occurred");
-          });
-      }}
-    >
-      {text}
-    </div>
-  );
+  return <div>{text}</div>;
 }
 
 export default Index;
